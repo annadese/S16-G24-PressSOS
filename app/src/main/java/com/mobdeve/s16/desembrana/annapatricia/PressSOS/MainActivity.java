a66798d00889c8d109e2f2f15b8645f9d29397bd
Anna Patricia Desembrana <anna_desembrana@dlsu.edu.ph>
2021-08-23 20:24:57 +0800

    Create project

---
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s16.desembrana.annapatricia.PressSOS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
