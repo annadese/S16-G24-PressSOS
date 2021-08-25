aa7af8622b9f43d52812574cbb7090d880b8b9b3
Anna Patricia Desembrana <anna_desembrana@dlsu.edu.ph>
2021-08-26 00:11:52 +0800

    Added navigation of fragments

--- app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/AccountFragment.java
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/AccountFragment.java
@@ -1,13 +1,14 @@
 package com.mobdeve.s16.desembrana.annapatricia.PressSOS;
 
-import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment{
+public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
--- app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/MainActivity.java
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/MainActivity.java
@@ -1,14 +1,67 @@
 package com.mobdeve.s16.desembrana.annapatricia.PressSOS;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+        setContentView(R.layout.nav_bar);
 
+        this.bottomNav = findViewById(R.id.bottomNavigationView);
+        Log.d("MAIN act", String.valueOf(bottomNav));
+        this.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.info_item:
+                        selectedFragment = new InfoFragment();
+                        break;
+                    case R.id.locationHistory_item:
+                        selectedFragment = new LocationHistoryFragment();
+                        break;
+                    case R.id.home_item:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.emergencyContacts_item:
+                        selectedFragment = new EmergencyContactsFragment();
+                        break;
+                    case R.id.account_item:
+                        selectedFragment = new AccountFragment();
+                        break;
+                }
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            bottomNav.setSelectedItemId(R.id.home_item); // change to whichever id should be default
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
     }
 }
