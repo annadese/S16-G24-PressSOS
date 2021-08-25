768d696871bd2898b4de1328ec03327c127ecd16
Clarissa Mandadero <clarissa_mandadero@dlsu.edu.ph>
2021-08-26 01:29:26 +0800

    accountfragment global variables

--- app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/AccountFragment.java
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/AccountFragment.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AccountFragment extends Fragment {
+
+    private Button btneditname, btneditnum, btneditsos, btnclear;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
