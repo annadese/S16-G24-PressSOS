58449c0bc74cd1fc339febf53482577d4b3ee1b6
Clarissa Mandadero <clarissa_mandadero@dlsu.edu.ph>
2021-09-17 02:43:54 +0800

    lat ang long

--- app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/HomeFragment.java
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/HomeFragment.java
@@ -1,8 +1,12 @@
 package com.mobdeve.s16.desembrana.annapatricia.PressSOS;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +18,10 @@ import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment{
@@ -22,12 +30,14 @@ public class HomeFragment extends Fragment{
     private MediaPlayer alarmSound;
     private DbHelper helper;
     ArrayList<Contact> contacts;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         helper = new DbHelper(getContext());
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         this.alarmSound = MediaPlayer.create(getActivity(), R.raw.alarm);
         this.btnSOS = (Button)view.findViewById(R.id.main_btnsos);
         this.btnAlarm = (Switch)view.findViewById(R.id.main_btnalarm);
@@ -35,10 +45,34 @@ public class HomeFragment extends Fragment{
         this.btnSOS.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 alarm();            // alarm sound is activated
+                getLocation();
                 sendTextMessage();  // SOS message is sent to all emergency contacts
             }
         });
         return view;
+    }
+
+    private void getLocation() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                //get location
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<android.location.Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if(location != null){
+                            Double lat = location.getLatitude();
+                            Double longt = location.getLongitude();
+
+                            //textView.setText(lat+ " , " + longt);
+                            Toast.makeText(getActivity(), lat + ", " + longt, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else{
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
     }
 
 
