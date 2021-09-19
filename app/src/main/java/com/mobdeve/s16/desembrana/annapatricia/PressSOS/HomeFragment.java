8f6849063f76a50ec8dda8f185a91c56038f0b99
Anna Patricia Desembrana <anna_desembrana@dlsu.edu.ph>
2021-09-19 20:38:13 +0800

    -Edited max length for SOS message

--- app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/HomeFragment.java
+++ app/src/main/java/com/mobdeve/s16/desembrana/annapatricia/PressSOS/HomeFragment.java
@@ -1,48 +1,29 @@
 package com.mobdeve.s16.desembrana.annapatricia.PressSOS;
 
-import android.Manifest;
-import android.app.ActivityManager;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.media.MediaPlayer;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.provider.ContactsContract;
-import android.telephony.SmsManager;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Switch;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
-import static androidx.core.content.ContextCompat.getSystemService;
 
 public class HomeFragment extends Fragment{
     private static final String TAG = "HomeFragment1";
-    private static final int INTERVAL = 5000;    // 5 minutes
+    private static final int INTERVAL = (1000 * 60) * 5;    // 5 minutes
 
     private Button btnSOS;
     private Switch btnAlarm;
